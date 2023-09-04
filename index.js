dc901232ad50349079c85b342647c4ef57dbe010
Siva1priya <119998311+Siva1priya@users.noreply.github.com>
2023-09-04 23:20:57 +0530

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function validate() {
     if (username === password && username.length > 1) {
       alert("Login Successful!");
       window.localStorage.setItem("loginStatus", true);
-      window.location.href = "orders.html"; 
+      window.location.href = "order.html"; 
     } else {
       alert("Please enter valid credentials!");
       window.location.href = "index.html"; 
